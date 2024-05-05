19259dd145d7daea2ad6220cb54a9c32bcd9e8c7
orjancarlsen <orjan.carlsen@gmail.com>
2024-05-06 00:01:46 +0200

    create classes for DataTransformer and Regressor

---
+++ data/data_tronsformer.py
@@ -0,0 +1,129 @@
+import numpy as np
+import pandas as pd
+
+from sklearn.preprocessing import MinMaxScaler
+
+
+class DataTransformer:
+    """
+    A class that represents a dataset over a period of time for a single stock.
+    Used to process the data and prepare it for a machine learning model.
+
+    Attributes
+    ----------
+    df : pd.DataFrame
+        DataFrame with stock data from path.
+    X_features : list
+        Chosen features used to perform training and predictions.
+    Y_features : list
+        Chosen features used as ground truth.
+    df_train_X : pd.DataFrame
+        DataFrame with features for training
+    df_train_Y : pd.DataFrame
+        DataFrame with ground truth labels for training
+    df_test_X : pd.DataFrame
+        DataFrame with features for testing
+    df_test_Y : pd.DataFrame
+        DataFrame with ground truth labels for testing
+    scales : dict
+        Mapping between the different features and their scales.
+        Needs to be used later for inversion of the scaling. 
+    X_train : np.ndarray
+        Feature training set
+    Y_test : np.ndarray
+        Ground truth training set
+    X_test : np.ndarray
+        Feature testing set
+    Y_test : np.ndarray
+        Ground truth testing set
+
+    Methods
+    -------
+    split(date)
+        Splits the dataset into fractions for both training and test
+        based on a date, and feature and target.
+    scale()
+        Scales the datasets, and stores mapping of scaling for inversion later.
+    create_train_and_test_data(n_days)
+        Separate the splitted data into complete ndarrays that can be used
+        for training and testing a machine learning model.
+    """
+
+    X_features = ['Open', 'High', 'Low', 'Close', 'Volume']
+    Y_features = ['Open', 'Close']
+
+    def __init__(self, path: str) -> None:
+        """
+        Parameters
+        ----------
+        path : str
+            Path to csv file with stock data.
+        """
+
+        self.df = pd.read_csv(path, parse_dates=['Date'])
+    
+    def split(self, date: str) -> None:
+        """
+        Splits the dataset into fractions for both training and testing
+        based on a date, and feature and target.
+        """
+        
+        df_train = self.df[self.df['Date'] < date]
+        df_test = self.df[self.df['Date'] >= date]
+
+        self.df_train_X = df_train[self.X_features]
+        self.df_train_Y = df_train[self.Y_features]
+        self.df_test_X = df_test[self.X_features]
+        self.df_test_Y = df_test[self.Y_features]
+    
+    def scale(self) -> None:
+        """
+        Scales the datasets, and stores mapping of scaling for inversion later.
+        """
+
+        self.scales = {}
+
+        # Apply normalization on features
+        scale_features = ['Open', 'High', 'Low', 'Close', 'Volume']
+        for feature in scale_features:
+            # Fit on training data column
+            scale = MinMaxScaler(feature_range=(0, 1)).fit(self.df_train_X[[feature]])
+            
+            self.df_train_X[feature] = scale.transform(self.df_train_X[[feature]])
+            self.df_test_X[feature] = scale.transform(self.df_test_X[[feature]])
+
+            # Transform test data column
+            if feature in self.Y_features:
+                self.df_train_Y[feature] = scale.transform(self.df_train_Y[[feature]])
+                self.df_test_Y[feature] = scale.transform(self.df_test_Y[[feature]])
+
+            # Store the scale for inversion later
+            self.scales[feature] = scale
+    
+    def create_train_and_test_data(self, n_days: int) -> None:
+        """
+        Separate the splitted data into complete ndarrays that can be used
+        for training and testing a machine learning model.
+
+        Parameters
+        ----------
+        n_days : int
+            Number of days used for prediction
+        """
+
+        # Create training data
+        X_train = []
+        Y_train = []
+        for i in range(n_days + 1, self.df_train_X.shape[0]):
+            X_train.append(self.df_train_X.iloc[i - 1 - n_days:i - 1])
+            Y_train.append(self.df_train_Y.iloc[i])
+        self.X_train = np.array(X_train)
+        self.Y_train = np.array(Y_train)
+
+        # Create testing data
+        df_test_inputs = pd.concat((self.df_train_X.iloc[-n_days:], self.df_test_X), axis=0)
+        X_test = []
+        for i in range(n_days + 1, df_test_inputs.shape[0]):
+            X_test.append(df_test_inputs.iloc[i - 1 - n_days:i - 1])
+        self.X_test = np.array(X_test)
+        self.Y_test = np.array(self.df_test_Y.copy())
---
+++ models/regressor.py
@@ -0,0 +1,137 @@
+import matplotlib.pyplot as plt
+from keras.models import Sequential, load_model
+from keras.layers import Dense, Dropout, LSTM
+
+from ..data.data_tronsformer import DataTransformer
+
+
+class Regressor:
+    """
+    Representation of a machine learning model performing a regression to
+    predict stock prices.
+
+    Attributes
+    ----------
+    name : str
+        Name of the regressor.
+    data : DataTransformer
+        Transormed data used for training and testing the model.
+    regressor : Sequential
+        Model with plain stack of layers where each layer has exactly
+        one input tensor and one output tensor.
+
+    Methods
+    -------
+    train()
+        Fit the parameters of the regressor to the training set.
+    predict()
+        Use the trained regressor to predict the label features for the test set.
+    save()
+        Save the model parameters to file.
+    load()
+        Load the model parameters from file.
+    """
+
+    epochs = 100
+    batch_size = 32
+
+    def __init__(self, name: str, data: DataTransformer) -> None:
+        """
+        Parameters
+        ----------
+        name : str
+            Name of the regressor, used for storing the model parameters.
+        data : DataTransformer
+            Transormed data used for training and testing the model.
+        """
+
+        self.name = name
+        self.data = data
+
+        optimizer = 'adam'
+        loss_function = 'mean_squared_error'
+
+        regressor = Sequential()
+
+        regressor.add(LSTM(units=50, return_sequences=True, input_shape=(self.data.X_train.shape[1], self.data.X_train.shape[2])))
+        regressor.add(Dropout(0.2))
+
+        regressor.add(LSTM(units=50, return_sequences=True))
+        regressor.add(Dropout(0.2))
+
+        regressor.add(LSTM(units=50, return_sequences=True))
+        regressor.add(Dropout(0.2))
+
+        regressor.add(LSTM(units=50))
+        regressor.add(Dropout(0.2))
+
+        regressor.add(Dense(units=self.data.Y_train.shape[1]))
+
+        regressor.compile(optimizer=optimizer, loss=loss_function)
+
+        self.regressor = regressor
+    
+    def train(self) -> None:
+        """
+        Fit the parameters of the regressor to the training set.
+        """
+
+        self.regressor.fit(
+            self.data.X_train, 
+            self.data.Y_train, 
+            epochs=self.epochs, 
+            batch_size=self.batch_size
+        )
+    
+    def predict(self) -> None:
+        """
+        Use the trained regressor to predict the label features for the test set.
+        """
+
+        self.Y_pred = self.regressor.predict(self.data.X_test)
+        self.Y_pred[:, 0] = self.data.scales['Open'].inverse_transform(self.Y_pred[:, 0].reshape(-1, 1)).reshape(-1)
+        self.Y_pred[:, 1] = self.data.scales['Close'].inverse_transform(self.Y_pred[:, 1].reshape(-1, 1)).reshape(-1)
+
+        self.Y_true = self.data.df_test_Y.copy()
+        self.Y_true['Open'] = self.data.scale_dict['Open'].inverse_transform(self.Y_true['Open'].values.reshape(-1, 1)).reshape(-1)
+        self.Y_true['Close'] = self.data.scale_dict['Close'].inverse_transform(self.Y_true['Close'].values.reshape(-1, 1)).reshape(-1)
+
+    
+    def plot(self, feature: str) -> None:
+        """
+        Plots predicted values against true values for one feature.
+
+        Parameters
+        ----------
+        feature : str
+            Feature's values to be plotted.
+
+        Raises
+        ------
+        Exception
+            If feature which is not predicted is tried to be plotted.
+        """
+        
+        if feature not in self.data.Y_features:
+            raise Exception(f'{feature} not found in {self.data.Y_features}')
+
+        plt.plot(self.Y_true[feature].values, '-', color='red', label=f'Real NOD Stock Price {feature}')
+        plt.plot(self.Y_pred[:, self.data.Y_features.index(feature)], '--', color='red', label=f'Predicted NOD Stock Price {feature}')
+        plt.title('NOD Stock Price Prediction')
+        plt.xlabel('Time')
+        plt.ylabel('NOD Stock Price')
+        plt.legend()
+
+    def save(self) -> None:
+        """
+        Save the model parameters to file.
+        """
+        
+        self.regressor.save(f'{self.name}.keras')
+    
+    def load(self) -> None:
+        """
+        Load the model parameters from file.
+        """
+
+        self.regressor = load_model(f'{self.name}.keras')
