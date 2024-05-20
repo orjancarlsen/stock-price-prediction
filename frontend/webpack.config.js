7b2f1c09f674ffe5110d903277754806397a930c
orjancarlsen <orjan.carlsen@gmail.com>
2024-05-20 22:21:55 +0200

    create simple frontend application

---
+++ frontend/webpack.config.js
@@ -0,0 +1,25 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"]
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./public/index.html",
+      filename: "./index.html"
+    })
+  ]
+};
